c95dd19df31d9407e2bd766a5ca058c48db2b86e
seyoung <seyoung0916@naver.com>
2020-12-03 14:38:16 +0900

    수정, 삭제 기능

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -3,6 +3,12 @@ var main = {
         var _this = this;
         $('#btn-save').on('click', function () {
             _this.save();
+        });
+        $('#btn-update').on('click', function () {
+            _this.update();
+        });
+        $('#btn-delete').on('click', function () {
+            _this.delete();
         })
     },
     save: function () {
@@ -24,6 +30,42 @@ var main = {
         }).fail(function (error) {
             alert(JSON.stringify(error));
         })
+    },
+    update: function () {
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function () {
+            alert('수정 완료');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    delete: function () {
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8'
+        }).done(function () {
+            alert('글 삭제됨');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
     }
 }
 main.init();
