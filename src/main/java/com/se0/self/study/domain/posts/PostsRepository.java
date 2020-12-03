5189b6a5c61995b91f6d6c1aa564cd5e898f1b29
seyoung <seyoung0916@naver.com>
2020-12-03 12:09:49 +0900

    index 페이지에 조회 추가

--- src/main/java/com/se0/self/study/domain/posts/PostsRepository.java
+++ src/main/java/com/se0/self/study/domain/posts/PostsRepository.java
@@ -1,6 +1,12 @@
 package com.se0.self.study.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p FROM Posts p order by p.id DESC")
+    List<Posts> findAllDesc();
 }
