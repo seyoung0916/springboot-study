45bb086591d14a5214c652d4a1cca741f7ab3adb
seyoung <seyoung0916@naver.com>
2020-12-04 16:19:42 +0900

    db를 세션 저장소로 사용

--- src/main/java/com/se0/self/study/config/auth/dto/SessionUser.java
+++ src/main/java/com/se0/self/study/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.se0.self.study.config.auth.dto;
 import com.se0.self.study.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
