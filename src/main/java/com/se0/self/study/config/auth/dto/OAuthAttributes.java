023cfc59689c488f0ce82d38bc27ed0d8f224108
seyoung <seyoung0916@naver.com>
2020-12-07 10:59:52 +0900

    네이버 로그인 연동

--- src/main/java/com/se0/self/study/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/se0/self/study/config/auth/dto/OAuthAttributes.java
@@ -26,15 +26,30 @@ public class OAuthAttributes {
 
     // OAuth2User에서 반환하는 사용자 정보가 map이기 때문에 값 하나하나를 변환해야 함
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
-        return  OAuthAttributes.builder()
+        return OAuthAttributes.builder()
                 .name((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("picture"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
